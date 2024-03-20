438c09b50ed3c2fa64af33ab4a13e279d59e0c2e
zlols <zlols@foxmail.com>
2024-03-21 01:21:44 +0800

    first

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+discription = "a package of utils"
+
+setuptools.setup(
+  name="zlutils",
+  version='0.1.0',
+  python_requires=">=3.6",
+  author="zlols",
+  author_email="zlols@foxmail.com",
+  description=discription,
+  long_description=discription,
+  long_description_content_type="text/markdown",
+  url="",
+  py_modules=['zlutils'],
+  install_requires=[
+    ],
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3 :: Only",
+  "License :: OSI Approved :: MIT License",
+  #"Operating System :: OS Independent",
+  ],
+)
